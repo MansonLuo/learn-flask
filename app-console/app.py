2863222a007bb7f7e1b1d51adc8d798578eaee27
MansonLuo <1558911620@qq.com>
2019-11-01 22:29:55 +0800

    reached to section flask-bootstrap within chapter 3

--- app-console/app.py
+++ app-console/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, redirect, abort
 import os
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
     images = os.listdir("./static/images")
     return render_template("index.html", images=images)
 
+if "__main__" == __name__:
+    app.run(host='0.0.0.0', port=5000, debug=True)
