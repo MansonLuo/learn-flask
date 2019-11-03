5dfab3b05df4e01e4bec016bf6aa2a41c2bf6bde
MansonLuo <1558911620@qq.com>
2019-11-03 20:50:04 +0800

    nothing

--- test/app.py
+++ test/app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = "secret key"
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('what is your name?', validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 if '__main__' == __name__:
     app.run(host='0.0.0.0', port=5000, debug=True)
