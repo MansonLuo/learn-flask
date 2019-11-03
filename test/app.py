12751f760b8b5a5fd93ba3b4192be6bbf16185f9
MansonLuo <1558911620@qq.com>
2019-11-03 19:22:53 +0800

    Add testCase for flask_moment to c3 branch

--- test/app.py
+++ test/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.route('/')
 def index():
-    return '<p>hello</p>'
+    return render_template('index.html')
+
+@app.route('/test/<testName>')
+def test(testName):
+    if "test-flask-moment" == testName:
+        return render_template('test-flask-moment.html', current_time = datetime.utcnow())
+    else:
+        return redirect(url_for('index'))
 
 if "__main__" == __name__:
-    app.run(host="0.0.0.0", host=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
