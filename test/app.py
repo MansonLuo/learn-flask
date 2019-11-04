402a730d9952f9fe5644927bd0e60e4736886fcb
MansonLuo <1558911620@qq.com>
2019-11-04 20:44:45 +0800

    Add  test for flashed messages to branch c4,and all c4 over.

--- test/app.py
+++ test/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, session
+from flask import Flask, render_template, url_for, redirect, session, flash
 from flask_bootstrap import Bootstrap
 from forms import NameForm
 
@@ -26,6 +26,14 @@ def test(testName):
 
             return redirect(url_for('test', testName='redirectAndUserSession', form=form, name=name))
         return render_template('redirectAndUserSession.html', form=form, name=session.get('name'))
+    elif 'flashedMessages' == testName:
+        if form.validate_on_submit():
+            old_name = session.get('name')
+            if old_name is not None and form.name.data != old_name:
+                flash("Look like you have changed your name!")
+            session['name'] = form.name.data
+            return redirect(url_for('test', testName='flashedMessages',form=form, name=session.get('name')))
+        return render_template('flashedMessages.html', form=form, name=session.get('name'))
 
 if '__main__' == __name__:
     app.run(host="0.0.0.0", port=5000, debug=True)
